f9c3b3ed20345e2274fa4e6d4b4ae84c9245fda6
mckinleyb <billmckinley@hotmail.com>
2015-11-15 09:58:23 -0600

    First commit

---
+++ user_add.sh
@@ -0,0 +1,40 @@
+#Bash script for Adding User Accounts
+
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+	echo "Only root may add a user to the system"
+        exit 1
+fi
+
+egrep "sudo" /etc/group > /dev/null
+		if [ $? -ne 0 ]; then
+		cp /etc/sudoers > /etc/sudoers.bkup
+		groupadd sudo
+		echo "Group 'sudo' added"
+		echo "Please ensure 'sudo' group has proper privileges"
+	else
+		echo "Group 'sudo' already exists"
+                echo "Please ensure 'sudo' group has proper privileges"
+		fi
+
+
+while read first_name last_name username email;
+	do egrep "^$username" /etc/passwd > /dev/null
+		if [ $? -eq 0 ]; then
+                echo "A user with username $username exists!"
+           else
+                useradd -G sudo -m $username
+                [ $? -eq 0 ] && echo "User $username has been added to system!" || echo "Failed to add user: $username"
+                echo $username | mail -s "Account ID for server $(hostname)" $email
+
+		pass=$(< /dev/urandom tr -dc A-Na-n1-9 | head -c8)
+		echo "$username:$pass" | chpasswd
+		[ $? -eq 0 ] && echo "User $username has updated password." || echo "Failed to update password for $username."
+		echo $pass | mail -s "New account created on $(hostname)" $email
+
+		chage -d 0 $username
+		fi
+        done < unix_list.txt
+
+exit 0
